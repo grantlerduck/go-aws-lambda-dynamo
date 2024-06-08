619f52bcacfe6a1d35062a194f23b13676fcedf0
grantlerduck <daniel.alker@outlook.de>
2024-06-09 00:09:04 +0200

    Test trigger.

--- constructs/codepipeline/pipeline.go
+++ constructs/codepipeline/pipeline.go
@@ -142,6 +142,7 @@ func addPRTrigger(pipeline pipelines.CodePipeline) {
 								jsii.String("feat/**"),
 								jsii.String("feature/**"),
 								jsii.String("chore/**"),
+								jsii.String("bug/**"),
 							},
 							Events: &[]awscodepipeline.GitPullRequestEvent{
 								awscodepipeline.GitPullRequestEvent_OPEN,
